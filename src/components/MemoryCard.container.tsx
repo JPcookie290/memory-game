5c19866c79fe8453e2c6bb24f98f334e4f14ff7e
JPcookie290 <julia.paier@yahoo.com>
2024-02-04 21:42:07 +0100

    inital memory-card file structure idea

---
+++ src/components/MemoryCard.container.tsx
@@ -0,0 +1,9 @@
+import MemoryCard from "./MemoryCard";
+import MemoryCardView from "./MemoryCardView";
+
+export default function MemoryCardContainer() {
+    return (
+        <>
+        </>
+    )
+}
